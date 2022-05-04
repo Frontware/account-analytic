59b77cde85f9fe875d6ed1a56fc809dc30ed5d28
kpo <kpo@frontware.com>
2022-05-04 14:00:33 +0700

    [FIX] website

--- analytic_base_department/__manifest__.py
+++ analytic_base_department/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "website": "https://github.com/OCA/account-analytic",
+    "website": "https://github.com/Frontware/account-analytic",
     "depends": ["account", "hr"],
     "data": ["views/analytic.xml", "views/hr_department_views.xml"],
     "installable": True,
